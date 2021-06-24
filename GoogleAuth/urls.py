07c171cf827d039cf997a5f39565f0627f2af872
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-25 03:39:43 +0530

    Add files via upload

---
+++ GoogleAuth/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name = "index.html")),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
+]
